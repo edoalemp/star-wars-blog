a3a2921d16699189b7eb969990c0ff7c71f22061
edoalemp <eduardo.munoz.p@gmail.com>
2019-06-20 20:26:44 +0000

    corregido vista de lista

--- src/js/layout.js
+++ src/js/layout.js
@@ -3,15 +3,11 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./views/home";
-import { Characters } from "./views/characters";
-import { Starships } from "./views/starships";
-import { Worlds } from "./views/worlds";
-import { Single } from "./views/single";
-import injectContext from "./store/appContext";
 import { List } from "./views/list";
-
+import { Single } from "./views/single";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import injectContext from "./store/appContext";
 
 //create your first component
 export class Layout extends React.Component {
